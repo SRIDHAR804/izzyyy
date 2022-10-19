7a163c572c5052c425be127715744d1c1b82c232
SRIDHAR804 <99116453+SRIDHAR804@users.noreply.github.com>
2022-10-19 17:53:09 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE NekoRobot
+TITLE one love music robot
 :: Enables virtual env mode and then starts Neko
-env\scripts\activate.bat && py -m NekoRobot
+env\scripts\activate.bat && py -m one love music robot
